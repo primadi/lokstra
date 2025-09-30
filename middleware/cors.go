0e9ae39470d2b6404d349932794915fbc3a0c8ef
Primadi Setiawan <primadisetiawan@gmail.com>
2025-09-30 17:12:19 +0700

    fix cors implementation

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,20 +11,27 @@ func Cors(allowOrigins []string) request.HandlerFunc {
 	AllOrigins := slices.Contains(allowOrigins, "*")
 	return request.HandlerFunc(func(c *request.Context) error {
 		origin := c.R.Header.Get("Origin")
+		// only set CORS headers if Origin header is present
 		if origin != "" {
+			// if not allowing all origins, check if origin is in the allowed list
 			if !AllOrigins && !slices.Contains(allowOrigins, origin) {
 				c.W.WriteHeader(http.StatusForbidden)
 				return nil
 			}
+
+			// Set CORS headers
 			c.W.Header().Set("Access-Control-Allow-Origin", origin)
 			c.W.Header().Set("Access-Control-Allow-Credentials", "true")
-		}
-		if c.R.Method == http.MethodOptions {
-			if reqHeaders := c.R.Header.Get("Access-Control-Request-Headers"); reqHeaders != "" {
-				c.W.Header().Set("Access-Control-Allow-Headers", reqHeaders)
+
+			// Handle preflight requests
+			if c.R.Method == http.MethodOptions {
+				if reqHeaders := c.R.Header.Get("Access-Control-Request-Headers"); reqHeaders != "" {
+					c.W.Header().Set("Access-Control-Allow-Headers", reqHeaders)
+				}
+				// Sets commonly used methods
+				c.W.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+				c.W.WriteHeader(http.StatusNoContent)
 			}
-			c.W.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			c.W.WriteHeader(http.StatusNoContent)
 		}
 		return c.Next()
 	})
